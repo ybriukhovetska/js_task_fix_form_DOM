3b158216bf11e522b20aadd994c0924fb1289f1c
yuliana111007-debug <yuliana111007@gmail.com>
2026-02-22 20:06:17 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
 'use strict';
 
-// write code here
+const forms = document.querySelectorAll('form');
+
+function capitalize(value) {
+  const word = value[0].toUpperCase() + value.slice(1);
+
+  return word;
+}
+
+for (const form of forms) {
+  const inputs = form.querySelectorAll('input');
+
+  for (const input of inputs) {
+    if (!input.id) {
+      input.id = input.name;
+    }
+
+    const label = document.createElement('label');
+
+    label.classList.add('field-label');
+
+    label.htmlFor = input.id;
+    label.textContent = capitalize(input.name);
+    input.placeholder = capitalize(input.name);
+    input.parentNode.appendChild(label);
+  }
+}
